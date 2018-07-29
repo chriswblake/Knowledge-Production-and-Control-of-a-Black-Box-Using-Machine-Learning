152e9923be1950da5a41f82f534475cc5ae35136
Christopher Blake <chriswblake@gmail.com>
2018-07-29 21:17:58 +0200

    First version of Discretizer finished.

--- Discretization/Discretization/Bin.cs
+++ Discretization/Discretization/Bin.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Discretization
 {
-    public class Bin : IComparable
+    public class Bin_orig
     {
         //Properties
-        int BinID { get; set; }
-        int Count { get; set; }
-        double Sum { get; set; }
-        double SquareSum { get; set; }
-        public double Average {
+        public int BinID { get; set; }
+        public int Count { get; set; }
+        public double Sum { get; set; }
+        public double SquareSum { get; set; }
+        public double Average
+        {
             get
             {
                 //Check for divide by zero
@@ -19,7 +22,7 @@ namespace Discretization
                 return Sum / Count;
             }
         }
-        double StandardDeviation
+        public double StandardDeviation
         {
             get
             {
@@ -34,43 +37,43 @@ namespace Discretization
             }
         }
 
-        //Properties - Range
-        /// <summary>
-        /// The lowest allowed value to be assigned to this bin. (inclusive)
-        /// </summary>
-        public double Low { get; private set; }
-        /// <summary>
-        /// The maximum value allowed to be assigned to this bin. (exclusive).
-        /// </summary>
-        public double High { get; private set; }
-        /// <summary>
-        /// Defines the accuracy requirement for values considered out of tolerance. A higher value means a larger
-        /// window around the average will be accepted as in tolerance.
-        /// 1 Standard Deviation  => 68.3%
-        /// 2 Standard Deviations => 95.5%
-        /// 3 Standard Deviations => 99.7%
-        /// 4 Standard Deviations => 99.9%
-        /// </summary>
+        public double Low { get; protected set; }
+        public double High { get; protected set; }
         public double NumStandardDeviations { get; set; }
-        /// <summary>
-        /// The number of values that have been assigned to this bin but were not within
-        /// the accepted tolerance range (number of standard deviations).
-        /// </summary>
+
         int CountOutTolerance { get; set; }
         int CountInTolerance { get; set; }
+        double HighInTolerance
+        {
+            get
+            {
+                return Average + NumStandardDeviations * StandardDeviation;
+            }
+        }
+        double LowInTolerance
+        {
+            get
+            {
+                return Average - NumStandardDeviations * StandardDeviation;
+            }
+        }
 
         //Constructors
-        public Bin() : this(double.NegativeInfinity, double.PositiveInfinity) { }
-        public Bin(double low, double high)
+        public Bin_orig() : this(double.NegativeInfinity, double.PositiveInfinity) { }
+        public Bin_orig(double low, double high)
         {
             this.Low = low;
             this.High = high;
-            this.NumStandardDeviations = 1;
+            this.NumStandardDeviations = 1; //Ideally slightly less than 33%. We want the data divided into 3 equal ranges.
         }
 
         //Methods
-        public void AddValue(double value)
+        public virtual void AddValue(double value)
         {
+            //Check if value is in range
+            if (value < this.Low || value >= this.High)
+                return;
+
             //Update statistics
             this.Count += 1;
             this.Sum += value;
@@ -86,8 +89,11 @@ namespace Discretization
         }
         public bool ShouldSplit()
         {
-            //If more values have been experienced out of the tolerance area, this bin should be split.
-            return (this.CountOutTolerance > 2 * this.CountInTolerance);
+            //If the out-tolerance data is double the in-tolerance data, that means we have potentially 3 areas.
+            if (this.Count > 50)
+                return (this.CountOutTolerance >= 2 * this.CountInTolerance);
+            else
+                return false;
         }
 
         //Debug
@@ -95,11 +101,168 @@ namespace Discretization
         {
             return string.Format("Avg:{0:N2} \tLow:{1:N2} \tHigh:{2:N2}", this.Average, this.Low, this.High);
         }
+    }
 
-        //Icomparable
-        public int CompareTo(object obj)
+    public class Bin
+    {
+        //Properties
+        public int BinID { get; set; }
+        public int Count { get; set; }
+        public double Sum { get; set; }
+        public double SquareSum { get; set; }
+        public double Average
         {
-            return this.Average.CompareTo(obj);
+            get
+            {
+                //Check for divide by zero
+                if (Count <= 0)
+                    return double.PositiveInfinity;
+
+                return Sum / Count;
+            }
+        }
+        public double StandardDeviation
+        {
+            get
+            {
+                //Check for divide by zero
+                if (Count <= 1)
+                    return double.PositiveInfinity;
+
+                //Compute standard deviation (N-1)
+                double numerator = (SquareSum) + (-2 * Average * Sum) + (Count * Math.Pow(Average, 2));
+                double denominator = Count - 1;
+                return Math.Sqrt(numerator / denominator);
+            }
+        }
+
+        public double Low { get; private set; }
+        public double High { get; private set; }
+        public double NumStandardDeviations { get; private set; }
+        public double AvgPlusNSigma { get
+            {
+                return this.Average + this.NumStandardDeviations * StandardDeviation;
+            }
+        }
+        public double AvgMinusNSigma
+        {
+            get
+            {
+                return this.Average - this.NumStandardDeviations * StandardDeviation;
+            }
+        }
+
+        public int Count1StdDev { get; set; }
+        public double Percent1StdDev { get
+            {
+                return Convert.ToDouble(Count1StdDev) / this.Count;
+            }
+        }
+
+        public int MinPointsForAction { get; set; }
+
+        //Constructors
+        public Bin() : this(double.NegativeInfinity, double.PositiveInfinity) { }
+        public Bin(double low, double high) : this(low, high, 6) { }
+        public Bin(double low, double high, double numStandardDeviations)
+        {
+            this.Low = low;
+            this.High = high;
+            this.NumStandardDeviations = numStandardDeviations;
+            this.MinPointsForAction = 100;
+        }
+
+        //Methods
+        public void AddValues(List<double> values)
+        {
+            foreach(double v in values)
+                AddValue(v);
+        }
+        public void AddValue(double value)
+        {
+            //Check if value is in range
+            if (value < this.Low || value >= this.High)
+                return;
+
+            //Update statistics
+            this.Count += 1;
+            this.Sum += value;
+            this.SquareSum += Math.Pow(value, 2);
+
+            //Track how many items fell in +-1 standard deviation. This will be used to check the shame of the gausian curve.
+            if ((this.Average - StandardDeviation < value) && (value < this.Average + StandardDeviation))
+                this.Count1StdDev += 1;
+
+            //Reset Statistics if count is very high
+            if (this.Count > 10000)
+            {
+                this.Count = this.Count / 10;
+                this.Sum = this.Sum / 10;
+                this.SquareSum = this.SquareSum / 100;
+            }
+                
+        }
+
+        public BinAction PickAction()
+        {
+            //Only provide a recommendation if the count is above the threshold.
+            if (this.Count < MinPointsForAction)
+                return BinAction.InsufficientData;
+
+            //Calculate high and low of the recorded data
+            double nSigmaLow = this.Average - NumStandardDeviations * StandardDeviation;
+            double nSigmaHigh = this.Average + NumStandardDeviations * StandardDeviation;
+
+            //No Action: If both ends within the bin range.
+            if(this.Low != double.NegativeInfinity && this.High != double.PositiveInfinity)
+                if (nSigmaLow > this.Low && nSigmaHigh < this.High)
+                    return BinAction.None;
+            
+
+            //Split: If both ends are outside the bin range.
+            if (nSigmaLow < this.Low && nSigmaHigh > this.High)
+                return BinAction.SplitAtAvg;
+            //Split: Low is -inf and +NSigma is below high
+            if (this.Low == double.NegativeInfinity && nSigmaHigh < this.High)
+                return BinAction.SplitAtNegNSigma;
+            //Split: -NSigma is above low and high is +inf
+            if (nSigmaLow > this.Low && this.High == double.PositiveInfinity)
+                return BinAction.SplitAtPosNSigma;
+            //Split: Distribution is too flat.
+            if (Percent1StdDev < 0.50) //Ideally this should be 68.1% since it is looking for a gaussian distribution.
+                return BinAction.SplitAtAvg;
+
+
+            //MergeHigh: Right end overlaps other bin.
+            if (nSigmaLow > this.Low && nSigmaHigh > this.High)
+                return BinAction.MergeHigh;
+            //MergeLow: Left end overlaps other bin.
+            if (nSigmaLow < this.Low && nSigmaHigh < this.High)
+                return BinAction.MergeLow;
+
+
+
+            //This should never occur
+            return BinAction.Undecided;
+        }
+
+        //Debug
+        public override string ToString()
+        {
+            return string.Format("Low:{0:N2} \tHigh:{1:N2} \tAvg:{2:N2} \t-NSimga:{3:N2} \t+NSigma:{4:N2} \tCount:{5:N2}", this.Low, this.High, this.Average, this.AvgMinusNSigma, this.AvgPlusNSigma, this.Count);
         }
     }
+
+    public enum BinAction
+    {
+        None,
+        SplitAtAvg,
+        SplitAtNegNSigma,
+        SplitAtPosNSigma,
+        MergeHigh,
+        MergeLow,
+        InsufficientData,
+        Undecided //Probably an error. Not good!
+    }
+
 }
--- Discretization/Discretization/Discretizer.cs
+++ Discretization/Discretization/Discretizer.cs
@@ -25,21 +25,127 @@ namespace Discretization
             //Add this value to that bin
             theBin.AddValue(value);
 
-            //Split the bin if needed
-            if (theBin.ShouldSplit())
-                SplitBin(theBin);
+            //Determine the action to take
+            switch (theBin.PickAction())
+            {
+                case BinAction.SplitAtAvg:
+                    SplitBin(theBin, theBin.Average);
+                    break;
+                case BinAction.SplitAtNegNSigma:
+                    { 
+                    double splitPoint = theBin.Average - 1.05 * theBin.NumStandardDeviations * theBin.StandardDeviation;
+                    SplitBin(theBin, splitPoint);
+                    }
+                    break;
+                case BinAction.SplitAtPosNSigma:
+                    { 
+                    double splitPoint = theBin.Average + 1.05 * theBin.NumStandardDeviations * theBin.StandardDeviation;
+                    SplitBin(theBin, splitPoint);
+                    }
+                    break;
+
+                case BinAction.MergeHigh:
+                    Bin binHigh = Bins.Find(p => p.Low == theBin.High);
+                    MergeBins(theBin, binHigh);
+                    break;
+                case BinAction.MergeLow:
+                    Bin binLow = Bins.Find(p => p.High == theBin.Low);
+                    MergeBins(binLow, theBin);
+                    break;
+            }
 
             //Return that bin as the selected bin
             return theBin;
         }
-        private void SplitBin(Bin theBin)
+        //public void PerformAction(Bin theBin)
+        //{
+        //    //Calculate high and low of the recorded data
+        //    double sigmaLow = theBin.Average - theBin.NumStandardDeviations * theBin.StandardDeviation;
+        //    double sigmaHigh = theBin.Average + theBin.NumStandardDeviations * theBin.StandardDeviation;
+
+        //    switch (theAction)
+        //    { 
+        //        case BinAction.Split:
+        //            //Initial bin
+        //            if (theBin.Low == double.NegativeInfinity && theBin.High == double.PositiveInfinity)
+        //            {
+        //                //Create 3 bins
+        //                Bin binLow = new Bin(double.NegativeInfinity, sigmaLow);
+        //                Bin binMid = new Bin(sigmaLow, sigmaHigh);
+        //                Bin binHigh = new Bin(sigmaHigh, double.PositiveInfinity);
+        //                Bins.AddRange(new Bin[] { binLow, binMid, binHigh});
+        //                Bins.Remove(theBin);
+        //                return;
+        //            }
+
+        //            //Both ends are outside the bin range.
+        //            if (sigmaLow < theBin.Low && sigmaHigh > theBin.High)
+        //            {
+        //                //Create  bins
+        //                Bin binLow = new Bin(theBin.Low, theBin.Average);
+        //                Bin binHigh = new Bin(theBin.Average, theBin.High);
+        //                Bins.AddRange(new Bin[] { binLow, binHigh });
+        //                Bins.Remove(theBin);
+        //                return;
+        //            }
+
+        //            break;
+        //    }
+        //}
+        public List<Bin> SplitBin(Bin theBin, double splitPoint)
         {
-            //Remove existing bin
+            //Validate split point
+            if (splitPoint <= theBin.Low || splitPoint >= theBin.High)
+                return new List<Bin>(); //Do nothing
+
+            //Create bins
+            Bin binLow = new Bin(theBin.Low, splitPoint, theBin.NumStandardDeviations) { MinPointsForAction = theBin.MinPointsForAction };
+            Bin binHigh = new Bin(splitPoint, theBin.High, theBin.NumStandardDeviations) { MinPointsForAction = theBin.MinPointsForAction };
+
+            //Check if data statistics can be kept
+            if (binLow.Low <= theBin.AvgMinusNSigma && binLow.High > theBin.AvgPlusNSigma)
+            {
+                binLow.Count = theBin.Count;
+                binLow.Sum = theBin.Sum;
+                binLow.SquareSum = theBin.SquareSum;
+            }
+            else if (binHigh.Low <= theBin.AvgMinusNSigma && binHigh.High > theBin.AvgPlusNSigma)
+            {
+                binHigh.Count = theBin.Count;
+                binHigh.Sum = theBin.Sum;
+                binHigh.SquareSum = theBin.SquareSum;
+            }
+
+            //Update Bins list
             Bins.Remove(theBin);
+            Bins.Add(binLow);
+            Bins.Add(binHigh);
+
+            //Return new bins
+            return new List<Bin> {binLow, binHigh};
+        }
+        public Bin MergeBins(Bin bin1, Bin bin2)
+        {
+            //Get new settings for bin
+            double newLow = Math.Min(bin1.Low, bin2.Low);
+            double newHigh = Math.Max(bin1.High, bin2.High);
+            double newStdDev = Math.Max(bin1.NumStandardDeviations, bin2.NumStandardDeviations);
+            int newMinPointsForAction = Math.Max(bin1.MinPointsForAction, bin2.MinPointsForAction);
+
+            //Create Bin
+            Bin combinedBin = new Bin(newLow, newHigh, newStdDev) { MinPointsForAction = newMinPointsForAction };
+
+            //Combine data
+            combinedBin.Count = bin1.Count + bin2.Count;
+            combinedBin.Sum = bin1.Sum + bin2.Sum;
+            combinedBin.SquareSum = bin1.SquareSum + bin2.SquareSum;
             
-            //Create new bins
-            Bins.Add(new Bin(theBin.Low, theBin.Average));
-            Bins.Add(new Bin(theBin.Average, theBin.High));   
+            //Update Bins list
+            Bins.Remove(bin1);
+            Bins.Remove(bin2);
+            Bins.Add(combinedBin);
+
+            return combinedBin;
         }
 
         //Debug
