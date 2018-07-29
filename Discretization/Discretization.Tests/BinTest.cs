305ef2a64b7f8a29d07837e1f6f17fe08a1ae724
Christopher Blake <chriswblake@gmail.com>
2018-07-29 21:19:04 +0200

    Added unit tests for 'Bin' and 'Discretizer' classes.

---
+++ Discretization/Discretization.Tests/BinTest.cs
@@ -0,0 +1,257 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+
+namespace Discretization.Tests
+{
+    public class BinTest
+    {   //MethodName_StateUnderTest_ExpectedBehavior
+
+        [Fact]
+        public void Bin_EmptyConstructorDefault_LowNegInfHighPosInf()
+        {
+            Bin theBin = new Bin();
+
+            Assert.Equal(double.NegativeInfinity, theBin.Low);
+            Assert.Equal(double.PositiveInfinity, theBin.High);
+        }
+        [Fact]
+        public void Bin_ConstructorWithValues_Low0High10()
+        {
+            Bin theBin = new Bin(0, 10);
+
+            Assert.Equal(0, theBin.Low);
+            Assert.Equal(10, theBin.High);
+        }
+
+        [Fact]
+        public void Sum_NonEmptyListOfValues_TwentyTwo()
+        {
+            Bin theBin = new Bin();
+            theBin.AddValues(new List<double> { 2, 2, 2, 3, 4, 4, 4 });
+
+            double result = theBin.Sum;
+
+            Assert.Equal(21.0, result);
+        }
+
+        [Fact]
+        public void SquareSum_NonEmptyListOfValues_SixtyNine()
+        {
+            Bin theBin = new Bin();
+            theBin.AddValues(new List<double> { 2, 2, 2, 3, 4, 4, 4 });
+
+            double result = theBin.SquareSum;
+
+            Assert.Equal(69.0, result);
+        }
+
+        [Fact]
+        public void Average_NoValues_PosInfinity()
+        {
+            Bin theBin = new Bin();
+
+            double result = theBin.Average;
+
+            Assert.Equal(double.PositiveInfinity, result);
+        }
+        [Fact]
+        public void Average_NonEmptyListOfValues_One()
+        {
+            Bin theBin = new Bin();
+            theBin.AddValues(new List<double> { 2, 2, 2, 3, 4, 4, 4 });
+
+            double result = theBin.Average;
+
+            Assert.Equal(3.0, result);
+        }
+
+        [Fact]
+        public void StandardDeviation_NoValues_Zero()
+        {
+            Bin theBin = new Bin();
+
+            double result = theBin.StandardDeviation;
+
+            Assert.Equal(double.PositiveInfinity, result);
+        }
+        [Fact]
+        public void StandardDeviation_NonEmptyListOfValues_One()
+        { 
+            Bin theBin = new Bin();
+            theBin.AddValues(new List<double> { 2, 2, 2, 3, 4, 4, 4 });
+
+            double result = theBin.StandardDeviation;
+
+            Assert.Equal(1.0, result);
+        }
+
+        [Fact]
+        public void PickAction_LessThanMinDataPoints_InsufficientData()
+        {
+            Bin theBin = new Bin(0, 10, 3);
+            theBin.MinPointsForAction = 10;
+            theBin.AddValues(new List<double> {
+                3.2,
+                4.0,
+                5.0,
+                6.0,
+                6.8,
+            });
+
+            BinAction result = theBin.PickAction();
+
+            Assert.Equal(BinAction.InsufficientData, result);
+        }
+        [Fact]
+        public void PickAction_AllValuesWithinNSigma_DoNothing()
+        {
+            // Test
+            // Avg-nSigma > Low
+            // Avg+nSigma < High
+
+            Bin theBin = new Bin(0, 10, 3);
+            theBin.MinPointsForAction = 3;
+            theBin.AddValues(new List<double> {
+                3.2,
+                4.0,
+                5.0,
+                6.0,
+                6.8,
+            });
+
+            BinAction result = theBin.PickAction();
+            
+            Assert.Equal(BinAction.None, result);
+        }
+        [Fact]
+        public void PickAction_NSigmaBelowLowAndNSigmaAboveHigh_Split()
+        {
+            // Test
+            // Avg-nSigma < Low
+            // Avg+nSigma > High
+
+            Bin theBin = new Bin(0, 10, 3);
+            theBin.MinPointsForAction = 3;
+            theBin.AddValues(new List<double> {
+                2.0,
+                3.0,
+                5.0,
+                7.0,
+                8.0,
+            });
+
+            BinAction result = theBin.PickAction();
+
+            Assert.Equal(BinAction.SplitAtAvg, result);
+        }
+        [Fact]
+        public void PickAction_LowEqualsNegInfAndNSigmaBelowHigh_Split()
+        {
+            // Test
+            // Low = -Inf
+            // Avg+nSigma < High
+
+            Bin theBin = new Bin(double.NegativeInfinity, 10, 3);
+            theBin.MinPointsForAction = 3;
+            theBin.AddValues(new List<double> {
+                1.0,
+                2.0,
+                3.0,
+                4.0,
+                5.0,
+            });
+
+            BinAction result = theBin.PickAction();
+
+            Assert.Equal(BinAction.SplitAtNegNSigma, result);
+        }
+        [Fact]
+        public void PickAction_NSigmaAboveLowAndHighEqualsPosInf_Split()
+        {
+            // Test
+            // Avg-nSigma > Low
+            // High = +Inf
+
+            Bin theBin = new Bin(0, double.PositiveInfinity, 3);
+            theBin.MinPointsForAction = 3;
+            theBin.AddValues(new List<double> {
+                5.0,
+                6.0,
+                7.0,
+                8.0,
+                9.0,
+            });
+
+            BinAction result = theBin.PickAction();
+
+            Assert.Equal(BinAction.SplitAtPosNSigma, result);
+        }
+        //[Fact]
+        //public void PickAction_LowEqualsNegInfAndHighEqualsPosInf_Split()
+        //{
+        //    // Test
+        //    // Low = -Inf
+        //    // High = +Inf
+
+        //    Bin theBin = new Bin(double.NegativeInfinity, double.PositiveInfinity, 3);
+        //    theBin.MinPointsForAction = 3;
+        //    theBin.AddValues(new List<double> {
+        //        2.0,
+        //        3.0,
+        //        5.0,
+        //        7.0,
+        //        8.0,
+        //    });
+
+        //    BinAction result = theBin.PickAction();
+
+        //    Assert.Equal(BinAction.Split, result);
+        //}
+        [Fact]
+        public void PickAction_NSigmaAboveLowAndNSigmaAboveHigh_MergeHigh()
+        {
+            // Test
+            // Avg-nSigma > Low
+            // Avg+nSigma > High
+
+            Bin theBin = new Bin(0, 10, 3);
+            theBin.MinPointsForAction = 3;
+            theBin.AddValues(new List<double> {
+                5.0,
+                6.0,
+                7.0,
+                8.0,
+                9.0,
+            });
+            theBin.Count1StdDev = theBin.Count; //This is to override the check for a flat distribution. Ideally, a better dataset should be used for testing.
+            
+            BinAction result = theBin.PickAction();
+
+            Assert.Equal(BinAction.MergeHigh, result);
+        }
+        [Fact]
+        public void PickAction_NSigmaBelowLowAndNSigmaBelowHigh_MergeLow()
+        {
+            // Test
+            // Avg-nSigma < Low
+            // Avg+nSigma < High
+
+            Bin theBin = new Bin(0, 10, 3);
+            theBin.MinPointsForAction = 3;
+            theBin.AddValues(new List<double> {
+                1.0,
+                2.0,
+                3.0,
+                4.0,
+                5.0,
+            });
+            theBin.Count1StdDev = theBin.Count; //This is to override the check for a flat distribution. Ideally, a better dataset should be used for testing.
+
+            BinAction result = theBin.PickAction();
+
+            Assert.Equal(BinAction.MergeLow, result);
+        }
+    }
+}
---
+++ Discretization/Discretization.Tests/DiscretizerTest.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Discretization.Tests
+{
+    public class DiscretizerTest
+    {
+        [Fact]
+        public void SplitBins_DataOnlyinHighRange_KeepStatistics()
+        {
+            Discretizer disc = new Discretizer();
+            disc.Bins.Clear();
+            Bin bin1 = new Bin(double.NegativeInfinity,-10, 3);
+            Bin bin2 = new Bin(-10, 10, 3);
+            Bin bin3 = new Bin(10, double.PositiveInfinity, 3);
+            bin2.AddValues(new List<double> {
+                2,
+                2,
+                2,
+                3,
+                4,
+                4,
+                4,
+            });
+            bin2.MinPointsForAction = 2;
+            disc.Bins.AddRange(new List<Bin> { bin1, bin2, bin3 });
+
+            List<Bin> results = disc.SplitBin(bin2, -7);
+            Bin resultBin1 = results[0];
+            Bin resultBin2 = results[1];
+
+            Assert.Equal(4, disc.Bins.Count);
+            Assert.Equal(3, resultBin2.Average);
+            Assert.Equal(1, resultBin2.StandardDeviation);
+            Assert.Equal(0, resultBin1.Count);
+        }
+        [Fact]
+        public void SplitBins_DataOnlyinLowRange_KeepStatistics()
+        {
+            Discretizer disc = new Discretizer();
+            disc.Bins.Clear();
+            Bin bin1 = new Bin(double.NegativeInfinity, -10, 3);
+            Bin bin2 = new Bin(-10, 10, 3);
+            Bin bin3 = new Bin(10, double.PositiveInfinity, 3);
+            bin2.AddValues(new List<double> {
+                2,
+                2,
+                2,
+                3,
+                4,
+                4,
+                4,
+            });
+            bin2.MinPointsForAction = 2;
+            disc.Bins.AddRange(new List<Bin> { bin1, bin2, bin3 });
+
+            List<Bin> results = disc.SplitBin(bin2, 7);
+            Bin resultBin1 = results[0];
+            Bin resultBin2 = results[1];
+
+            Assert.Equal(4, disc.Bins.Count);
+            Assert.Equal(3, resultBin1.Average);
+            Assert.Equal(1, resultBin1.StandardDeviation);
+            Assert.Equal(0, resultBin2.Count);
+        }
+        [Fact]
+        public void SplitBins_DataAcrossNewRanges_LoseStatistics()
+        {
+            Discretizer disc = new Discretizer();
+            disc.Bins.Clear();
+            Bin bin1 = new Bin(double.NegativeInfinity, 0, 3);
+            Bin bin2 = new Bin(0, 10, 3);
+            Bin bin3 = new Bin(10, double.PositiveInfinity, 3);
+            bin2.AddValues(new List<double> {
+                2,
+                2,
+                2,
+                3,
+                4,
+                4,
+                4,
+            });
+            bin2.MinPointsForAction = 2;
+            disc.Bins.AddRange(new List<Bin> { bin1, bin2, bin3 });
+
+            List<Bin> results = disc.SplitBin(bin2, 3);
+            Bin resultBin1 = results[0];
+            Bin resultBin2 = results[1];
+
+            Assert.Equal(0, resultBin1.Low);
+            Assert.Equal(3, resultBin1.High);
+            Assert.Equal(3, resultBin2.Low);
+            Assert.Equal(10, resultBin2.High);
+
+            Assert.Equal(4, disc.Bins.Count);
+            Assert.Equal(0, resultBin1.Count);
+            Assert.Equal(0, resultBin2.Count);
+        }
+        [Fact]
+        public void MergeBins_TwoBinsWithValues_OneLessBinWithCombinedStatistics()
+        {
+            Discretizer disc = new Discretizer();
+            disc.Bins.Clear();
+            Bin bin1 = new Bin(double.NegativeInfinity, 0, 3);
+            Bin bin2 = new Bin(0, 2.5, 3);          
+            Bin bin3 = new Bin(2.5, double.PositiveInfinity, 3);
+            bin2.AddValues(new List<double> {
+                2,
+                2,
+                2,
+            });
+            bin2.MinPointsForAction = 2;
+            bin3.AddValues(new List<double> {
+                3,
+                4,
+                4,
+                4,
+            });
+            bin3.MinPointsForAction = 3;
+            disc.Bins.AddRange(new List<Bin> {bin1, bin2, bin3});
+
+            Bin combinedBin = disc.MergeBins(bin2, bin3);
+
+            Assert.Equal(2, disc.Bins.Count);
+            Assert.Equal(3, combinedBin.Average);
+            Assert.Equal(1, combinedBin.StandardDeviation);
+            Assert.Equal(0, combinedBin.Low);
+            Assert.Equal(double.PositiveInfinity, combinedBin.High);
+            Assert.Equal(3, combinedBin.MinPointsForAction);
+
+        }
+        [Fact]
+        public void GetBin_100RepeatedNoisyValues_102Bins()
+        {
+            //Values to generate from
+            var x_source = Enumerable.Range(0, 100).ToArray();
+
+            //Create list of values with noise
+            Random rand = new Random();
+            double noise = 0.01;
+            List<double> x_noisy = new List<double>();
+            for (int i = 0; i < 100; i++)
+            {
+                foreach (int x in x_source)
+                {
+                    //Generate random number with a flat distribution
+                    //double relNoise = 2 * noise * rand.NextDouble();
+                    //x_noisy.Add((x - noise) + relNoise);
+
+                    //Generate random number with rough gaussian distribution
+                    double u1 = 1.0 - rand.NextDouble(); //uniform(0,1] random doubles
+                    double u2 = 1.0 - rand.NextDouble();
+                    double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+
+                    //Pick Sign
+                    double sign = 1;
+                    if (rand.NextDouble() < 0.5)
+                        sign = -1;
+                    //Create Point
+                    x_noisy.Add(x + sign * randStdNormal * noise);
+                }
+            }
+            
+
+            //Add all values to the discretizer
+            Discretizer disc = new Discretizer();
+            for(int i =0; i<5; i++)
+                foreach (double x in x_noisy)
+                    disc.GetBin(x);
+
+            //Check
+            Assert.InRange(disc.Bins.Count,100,105); //Ideally 102, but there is a randomization factor so it may occasionally go higher or lower.
+        }
+    }
+}
