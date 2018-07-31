a64303d5b9eca11b19e678fea7c1d654cae237ae
Christopher Blake <chriswblake@gmail.com>
2018-07-31 17:32:20 +0200

    Added test for binary dataset at values of 0 and 5.

--- Discretization/Discretization.Tests/DiscretizerTest.cs
+++ Discretization/Discretization.Tests/DiscretizerTest.cs
@@ -98,6 +98,7 @@ namespace Discretization.Tests
             Assert.Equal(0, resultBin1.Count);
             Assert.Equal(0, resultBin2.Count);
         }
+
         [Fact]
         public void MergeBins_TwoBinsWithValues_OneLessBinWithCombinedStatistics()
         {
@@ -131,6 +132,7 @@ namespace Discretization.Tests
             Assert.Equal(3, combinedBin.MinPointsForAction);
 
         }
+
         [Fact]
         public void GetBin_100RepeatedNoisyValues_102Bins()
         {
@@ -172,6 +174,49 @@ namespace Discretization.Tests
 
             //Check
             Assert.InRange(disc.Bins.Count,100,105); //Ideally 102, but there is a randomization factor so it may occasionally go higher or lower.
+        }
+        [Fact]
+        public void GetBin_BinaryRepeatedNoisyValues_4Bins()
+        {
+            //Values to generate from
+            //var x_source = Enumerable.Range(0, 100).ToArray();
+            var x_source = new double[2] {0, 5};
+
+            //Create list of values with noise
+            Random rand = new Random();
+            double noise = 0.01;
+            List<double> x_noisy = new List<double>();
+            for (int i = 0; i < 100; i++)
+            {
+                foreach (int x in x_source)
+                {
+                    //Generate random number with a flat distribution
+                    //double relNoise = 2 * noise * rand.NextDouble();
+                    //x_noisy.Add((x - noise) + relNoise);
+
+                    //Generate random number with rough gaussian distribution
+                    double u1 = 1.0 - rand.NextDouble(); //uniform(0,1] random doubles
+                    double u2 = 1.0 - rand.NextDouble();
+                    double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+
+                    //Pick Sign
+                    double sign = 1;
+                    if (rand.NextDouble() < 0.5)
+                        sign = -1;
+                    //Create Point
+                    x_noisy.Add(x + sign * randStdNormal * noise);
+                }
+            }
+
+
+            //Add all values to the discretizer
+            Discretizer disc = new Discretizer();
+            for (int i = 0; i < 5; i++)
+                foreach (double x in x_noisy)
+                    disc.GetBin(x);
+
+            //Check
+            Assert.Equal(4, disc.Bins.Count); //Ideally 102, but there is a randomization factor so it may occasionally go higher or lower.
         }
     }
 }
