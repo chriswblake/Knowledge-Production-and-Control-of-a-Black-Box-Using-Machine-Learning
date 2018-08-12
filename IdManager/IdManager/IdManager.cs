cfa3d8c74b11fdb2d26f0e1d1aa1349997598e3f
Christopher Blake <chriswblake@gmail.com>
2018-08-12 17:36:18 +0200

    Initial commit of IdManager.

---
+++ IdManager/IdManager/IdManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace IdManager
+{
+    public static class IdManager
+    {
+        //Properties
+        private static int LastUsedID { get; set; }
+        private static object newIdLock = new object();
+
+        //Constructors
+        static IdManager()
+        {
+            //Load previous id information
+            LastUsedID = 0;
+        }
+
+        //Methods
+        public static int GetNewId()
+        {
+            lock(newIdLock)
+            {
+                LastUsedID++;
+                return LastUsedID; 
+            }
+        }
+    }
+}
