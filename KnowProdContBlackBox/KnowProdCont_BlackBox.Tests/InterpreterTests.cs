191aeda3bcc5d09c244fe6584dcf5bd3de3955af
Christopher Blake <chriswblake@gmail.com>
2018-08-31 15:25:23 +0300

    Added Interpreter

---
+++ KnowProdContBlackBox/KnowProdCont_BlackBox.Tests/InterpreterTests.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using KnowProdContBlackBox;
+using Discretization;
+using BlackBoxModeling;
+using KnowledgeProduction;
+using BlackBoxModeling;
+using IdManagement;
+using static Discretization.DataGeneration;
+using System.Threading;
+
+namespace KnowProdContBlackBox.Tests
+{
+    public class InterpreterTests
+    {
+        [Theory]
+        [InlineData(300)]
+        public void Learn_OnOffPattern_Count4(int iterations)
+        {
+            List<double> bool1 = new List<double> {
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+
+                    5.0,
+                    5.0,
+                    5.0,
+                    5.0,
+
+                    5.0,
+                    5.0,
+                    5.0,
+                    5.0,
+            };
+            List<double> bool2 = new List<double> {
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+
+                    5.0,
+                    5.0,
+                    5.0,
+                    5.0,
+
+                    5.0,
+                    5.0,
+                    5.0,
+                    5.0,
+
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+            };
+            IdManager idManager = new IdManager();
+            BlackBox logicBlackBox = new BlackBoxModeling.Samples.Logic() { TimeInterval_ms = 10 };
+            DiscreteBlackBox discBlackBox = new DiscreteBlackBox(logicBlackBox, idManager);
+            ProducerBlackBox prodBlackBox = new ProducerBlackBox(discBlackBox, idManager);
+            Interpreter interpreter = new Interpreter(prodBlackBox) { MemorySize = iterations * 2 };
+            Random rand = new Random();
+            logicBlackBox.Start();
+
+            #region Train
+            for (int i = 0; i < iterations; i++)
+            {
+                //Change input
+                double bool1_crisp = bool1[i % bool1.Count];
+                double bool2_crisp = bool2[i % bool2.Count];
+                logicBlackBox.Input["bool1"] = GenerateNoisyValue(rand, bool1_crisp, 0.001);
+                logicBlackBox.Input["bool2"] = GenerateNoisyValue(rand, bool2_crisp, 0.001);
+
+                //Wait until next sample time
+                Thread.Sleep(logicBlackBox.TimeInterval_ms);
+            }
+            #endregion
+
+            #region Assert
+            List<KnowInstance> vocabBool1 = prodBlackBox.Producers["bool1"].KnowInstances.Values.OrderBy(p => p.ID).ToList();
+            List<KnowInstance> memoryBool1 = interpreter.MemoryInput.Select(p => p["bool1"]).ToList();
+            foreach(KnowInstance ki in vocabBool1)
+            {
+                //Skip bins that have never been used.
+                if(ki.GetType() == typeof(KnowInstanceValue))
+                {
+                    Bin bin = (Bin)ki.Content;
+                    if (bin.Count == 0)
+                        continue;
+                }
+
+                //Check that all vocabulary appear in the memory.
+                Assert.Contains(ki, memoryBool1);
+            }
+            #endregion
+
+        }
+    }
+}
---
+++ KnowProdContBlackBox/KnowProdCont_BlackBox/Interpreter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discretization;
+using System.Threading;
+using KnowledgeProduction;
+using IdManagement;
+using BlackBoxModeling;
+using System.Linq;
+
+namespace KnowProdContBlackBox
+{
+    public class Interpreter
+    {
+        //Fields
+        private ProducerBlackBox prodBlackBox;
+
+        //Properties - IO state
+        public Queue<Dictionary<string, KnowInstance>> MemoryInput { get; set; } = new Queue<Dictionary<string, KnowInstance>>();
+        public Queue<Dictionary<string, KnowInstance>> MemoryOutput { get; set; } = new Queue<Dictionary<string, KnowInstance>>();
+        public int MemorySize { get; set; } = 20;
+        private Thread samplingThread;
+
+        //Properties - Interpratations
+        //private Producer prodHighInput;
+        //private Producer prodHighOutput;
+        //public Queue<KnowInstance> MemoryInterpretationsInput = new Queue<KnowInstance>();
+        //public Queue<KnowInstance> MemoryInterpretationsOutput = new Queue<KnowInstance>();
+
+        //Constructors
+        public Interpreter(ProducerBlackBox prodBlackBox)
+        {
+            this.prodBlackBox = prodBlackBox;
+
+            //Create thread for sampling and interpreting inputs and outputs
+            this.samplingThread = CreateSamplingThread();
+            prodBlackBox.OnStarting += ProdBlackBox_OnStarting; //Start when black box starts
+            
+        }
+
+        //Methods
+        private void AddToMemory(Dictionary<string, KnowInstance> inputState, Dictionary<string, KnowInstance> outputState)
+        {
+            //Check memory length
+            if (MemoryInput.Count == MemorySize)
+            {
+                MemoryInput.Dequeue();
+                MemoryOutput.Dequeue();
+            }
+
+            //Add the new item
+            MemoryInput.Enqueue(inputState);
+            MemoryOutput.Enqueue(outputState);
+        }
+        private Dictionary<string, KnowInstance> Interpret(Dictionary<string, KnowInstance> state1, Dictionary<string, KnowInstance> state2)
+        {
+            //if first state is null, just return second state
+            if (state1 == null)
+                return state2;
+
+            //Combine values and run through producer for interpretation.
+            Dictionary<string, KnowInstance> ioInterpretation = new Dictionary<string, KnowInstance>(state2);
+            foreach (string ioName in state1.Keys)
+            {
+                Producer prod = prodBlackBox.Producers[ioName];
+                KnowInstance k1 = state1[ioName];
+                KnowInstance k2 = state2[ioName];
+                KnowInstance interpretation = prod.Get(k1, k2);
+                if (interpretation != null)
+                    ioInterpretation[ioName] = interpretation;
+            }
+
+            return ioInterpretation;
+        }
+        private Thread CreateSamplingThread()
+        {
+            //Create the background sampling thread
+            Thread samplingThread = new Thread
+            (delegate ()
+            {
+                while (Thread.CurrentThread.IsAlive)
+                {
+                    //Sample values of inputs and outputs
+                    var inputState = prodBlackBox.Input.ToDictionary(d => d.Key, d => (KnowInstance)d.Value);
+                    var outputState = prodBlackBox.Output.ToDictionary(d => d.Key, d => (KnowInstance)d.Value);
+                    AddToMemory(inputState, outputState);
+
+                    //Send through interpreter
+                    var inputStateInter = Interpret(_prevInputState, inputState);
+                    var outputStateInter = Interpret(_prevOutputState, outputState);
+                    AddToMemory(inputStateInter, outputStateInter);
+
+                    //Update prev state
+                    _prevInputState = inputState;
+                    _prevOutputState = outputState;
+
+                    //Wait until next sample time
+                    Thread.Sleep(prodBlackBox.TimeInterval_ms);
+                }
+            });
+            samplingThread.IsBackground = true;
+
+            return samplingThread;
+        }
+
+        //Events
+        private void ProdBlackBox_OnStarting(object sender, EventArgs e)
+        {
+            samplingThread.Start();
+        }
+
+        //Methods - cache
+        private Dictionary<string, KnowInstance> _prevInputState = null;
+        private Dictionary<string, KnowInstance> _prevOutputState = null;
+    }
+}
