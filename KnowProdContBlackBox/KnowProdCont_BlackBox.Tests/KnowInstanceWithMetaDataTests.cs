3801986ccd018e7d27b2d12af1b2531beaa74063
Christopher Blake <chriswblake@gmail.com>
2018-09-02 15:31:19 +0300

    Added wrapper for KnowInstance. The wrapper retrieves metadata about an ID number.

---
+++ KnowProdContBlackBox/KnowProdCont_BlackBox.Tests/KnowInstanceWithMetaDataTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+using IdManagement;
+using KnowledgeProduction;
+using KnowProdContBlackBox;
+using RLDT;
+
+namespace KnowProdContBlackBox.Tests
+{
+    public class KnowInstanceWithMetaDataTests
+    {
+        [Fact]
+        public void Constructors_AreEqual_SameHashCodes()
+        {
+            IdManager idManager = new IdManager();
+            var ki = new KnowInstanceValue(5, 5.0);
+            var kim1 = new KnowInstanceWithMetaData(ki, idManager);
+            idManager.SetName(5, "On");
+            var kim2 = new KnowInstanceWithMetaData(ki, idManager);
+
+            var fvp1 = new FeatureValuePair("input1", ki);
+            var fvpm1 = new FeatureValuePair("input1", kim1);
+            var fvpm2 = new FeatureValuePair("input1", kim2);
+
+            var result1 = fvp1.GetHashCode();
+            var result2 = fvpm1.GetHashCode();
+            var result3 = fvpm2.GetHashCode();
+
+            Assert.Equal(fvp1.GetHashCode(), fvpm1.GetHashCode());
+            Assert.Equal(fvpm1, fvpm2);
+        }
+    }
+}
---
+++ KnowProdContBlackBox/KnowProdCont_BlackBox/KnowInstanceWithMetaData.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KnowledgeProduction;
+using IdManagement;
+
+namespace KnowProdContBlackBox
+{
+    public class KnowInstanceWithMetaData : IComparable
+    {
+        //Fields
+        public IdManager InnerIdManager { get; private set; }
+        public KnowInstance InnerKnowInstance { get; private set; }
+
+        //Properties
+        public int ID
+        {
+            get { return InnerKnowInstance.ID; }
+        }
+        public string Name
+        {
+            get
+            {
+                return InnerIdManager.GetName(InnerKnowInstance.ID);
+            }
+            set
+            {
+                InnerIdManager.SetName(InnerKnowInstance.ID, value);
+            }
+        }
+        public string IdAndName
+        {
+            get
+            {
+                return string.Format("({0}) {1}", InnerKnowInstance.ID, InnerIdManager.GetName(InnerKnowInstance.ID));
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return InnerIdManager.GetDescription(InnerKnowInstance.ID);
+            }
+            set
+            {
+                InnerIdManager.SetDescription(InnerKnowInstance.ID, value);
+            }
+        }
+        public string AdditionalNotes
+        {
+            get
+            {
+                return InnerIdManager.GetAdditionalNotes(InnerKnowInstance.ID);
+            }
+            set
+            {
+                InnerIdManager.SetAdditionalNotes(InnerKnowInstance.ID, value);
+            }
+        }
+
+        //Constructor
+        public KnowInstanceWithMetaData(KnowInstance knowInstance, IdManager idManager)
+        {
+            this.InnerKnowInstance = knowInstance;
+            this.InnerIdManager = idManager;
+        }
+
+        //Methods
+        public override string ToString()
+        {
+            return InnerKnowInstance.ToString();
+        }
+        public int CompareTo(object obj)
+        {
+            KnowInstanceWithMetaData that = (KnowInstanceWithMetaData)obj;
+            if (this.ID > that.ID)
+                return 1;
+            if (this.ID < that.ID)
+                return -1;
+            else
+                return 0;
+        }
+        public override int GetHashCode()
+        {
+            return InnerKnowInstance.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            return (this.GetHashCode() == obj.GetHashCode());
+        }
+    }
+}
