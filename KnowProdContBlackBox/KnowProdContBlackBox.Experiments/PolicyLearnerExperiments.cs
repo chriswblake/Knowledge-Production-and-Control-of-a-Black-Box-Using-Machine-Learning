b3ecc4c74f167f04ec71104a7bd4859daa55a242
Christopher Blake <chriswblake@gmail.com>
2018-09-04 11:40:40 +0300

    Added project for experiments. - Initial project includes learner for example logic operations (and/or/xor).

--- KnowProdContBlackBox/KnowProdContBlackBox.Experiments/PolicyLearnerExperiments.cs
+++ KnowProdContBlackBox/KnowProdContBlackBox.Experiments/PolicyLearnerExperiments.cs
@@ -16,9 +16,9 @@ namespace KnowProdContBlackBox.Experiments
     public class PolicyLearnerExperiments : Experiment
     {
         [Theory]
-        [InlineData(100)]
+        [InlineData(500)]
         [InlineData(1000)]
-        [InlineData(5000)]
+        [InlineData(1500)]
         public void LogicOperations(int iterations)
         {
             List<double> bool1 = new List<double> {
@@ -86,64 +86,112 @@ namespace KnowProdContBlackBox.Experiments
             }
             #endregion
 
-            //Decision Trees (detailed and simple)
-            var treeSettingsBlanksSubScores = new RLDT.DecisionTree.TreeSettings() { ShowBlanks = true, ShowSubScores = true };
-            var treeSettingsBlanks = new RLDT.DecisionTree.TreeSettings() { ShowBlanks = true, ShowSubScores = false };
-            var treeSettingsSimple = new RLDT.DecisionTree.TreeSettings() { ShowBlanks = false, ShowSubScores = false };
-            var treeDisplaySettings = new RLDT.DecisionTree.TreeNode.TreeDisplaySettings() { ValueDisplayProperty = "ID", LabelDisplayProperty = "ID" };
-            string htmlTree_BlanksSubScores = policyLearner.Policies["xor"].ToDecisionTree(treeSettingsBlanksSubScores).ToHtmlTree(treeDisplaySettings);
-            string htmlTree_Blanks = policyLearner.Policies["xor"].ToDecisionTree(treeSettingsBlanks).ToHtmlTree(treeDisplaySettings);
-            string htmlTree_Simple = policyLearner.Policies["xor"].ToDecisionTree(treeSettingsSimple).ToHtmlTree();// treeDisplaySettings);
-            string htmlTreeStyle = RLDT.DecisionTree.TreeNode.DefaultStyling;
+            //Get Discretizers and Producers
+            var disc1 = discBlackBox.Discretizers["bool1"];
+            var disc2 = discBlackBox.Discretizers["bool2"];
+            var discand = discBlackBox.Discretizers["and"];
+            var discor = discBlackBox.Discretizers["or"];
+            var discxor = discBlackBox.Discretizers["xor"];
+            var prod1 = prodBlackBox.Producers["bool1"];
+            var prod2 = prodBlackBox.Producers["bool2"];
+            var prodand = prodBlackBox.Producers["and"];
+            var prodor = prodBlackBox.Producers["or"];
+            var prodxor = prodBlackBox.Producers["xor"];
+            Policy policyand = policyLearner.Policies["and"];
+            Policy policyor = policyLearner.Policies["or"];
+            Policy policyxor = policyLearner.Policies["xor"];
 
-            //Tables of vocabulary
-            string htmlBool1 = "bool1 Vocabulary" + HtmlTools.ToHtmlTable(prodBlackBox.Producers["bool1"].KnowInstances.Values.ToList());
-            string htmlBool2 = "bool2 Vocabulary" + HtmlTools.ToHtmlTable(prodBlackBox.Producers["bool2"].KnowInstances.Values.ToList());
-            string htmlXor = "xor Vocabulary" + HtmlTools.ToHtmlTable(prodBlackBox.Producers["xor"].KnowInstances.Values.ToList());
-            string htmlVocab = string.Format(@"
-            <table>
-            <tr>
-            <td>{0}</td>
-            <td>{1}</td>
-            <td>{2}</td>
-            </tr>
-            </table>
-            ", htmlBool1, htmlBool2, htmlXor);
+            //Check if training was sucessfull
+            Assert.Equal(4, disc1.Bins.Count);
+            Assert.Equal(4, disc2.Bins.Count);
+            Assert.Equal(4, discand.Bins.Count);
+            Assert.Equal(4, discor.Bins.Count);
+            Assert.Equal(4, discxor.Bins.Count);
+            Assert.Equal(4, discxor.Bins.Count);
+            Assert.InRange(prod1.KnowInstances.Count, 8, 10);
+            Assert.InRange(prod2.KnowInstances.Count, 8, 10);
+            Assert.InRange(prodand.KnowInstances.Count, 8, 10);
+            Assert.InRange(prodor.KnowInstances.Count, 8, 10);
+            Assert.InRange(prodxor.KnowInstances.Count, 8, 10);
 
-            //Save to files
-            File.WriteAllText(Path.Combine(this.ResultsDir, "LogicOperations", iterations+"xorDetailed.html"), htmlTreeStyle + htmlVocab + htmlTree_BlanksSubScores);
-            File.WriteAllText(Path.Combine(this.ResultsDir, "LogicOperations", iterations + "xorBlanks.html"), htmlTreeStyle + htmlVocab + htmlTree_Blanks);
-            File.WriteAllText(Path.Combine(this.ResultsDir, "LogicOperations", iterations + "xorSimple.html"), htmlTreeStyle + htmlVocab + htmlTree_Simple);
-
-            return;
-
-            #region Assert
-            //Find equivalent zero and five for both inputs
-            var binBool1_0 = discBlackBox.Discretizers["bool1"].GetBin(0.0);
-            var binBool1_5 = discBlackBox.Discretizers["bool1"].GetBin(5.0);
-            var bool1_0 = prodBlackBox.Producers["bool1"].Get(binBool1_0.BinID);
-            var bool1_5 = prodBlackBox.Producers["bool1"].Get(binBool1_5.BinID);
-
-            var binBool2_0 = discBlackBox.Discretizers["bool2"].GetBin(0.0);
-            var binBool2_5 = discBlackBox.Discretizers["bool2"].GetBin(5.0);
-            var bool2_0 = prodBlackBox.Producers["bool2"].Get(binBool2_0.BinID);
-            var bool2_5 = prodBlackBox.Producers["bool2"].Get(binBool2_5.BinID);
-
-            DataVector dataVectorTrue = new DataVector(new string[] { "bool1", "bool2" }, new object[] { bool1_5, bool2_5 });
-            DataVector dataVectorFalse = new DataVector(new string[] { "bool1", "bool2" }, new object[] { bool1_0, bool2_5 });
-
-            var result1 = policyLearner.Policies["and"].Classify_ByPolicy(dataVectorTrue);
-            var result2 = policyLearner.Policies["and"].Classify_ByPolicy(dataVectorFalse);
-
+            //Name Entities
+            #region bool1
+            KnowInstance bool1_On = prod1.Get(disc1.GetBin(5.0).BinID);
+            KnowInstance bool1_Off = prod1.Get(disc1.GetBin(0.0).BinID);
+            KnowInstance bool1_StayOff = prod1.Get(bool1_Off, bool1_Off);
+            KnowInstance bool1_StayOn = prod1.Get(bool1_On, bool1_On);
+            KnowInstance bool1_SwitchOff = prod1.Get(bool1_On, bool1_Off);
+            KnowInstance bool1_SwitchOn = prod1.Get(bool1_Off, bool1_On);
+            idManager.SetName(bool1_On.ID, "True");
+            idManager.SetName(bool1_Off.ID, "False");
+            idManager.SetName(bool1_StayOff.ID, "Stay False");
+            idManager.SetName(bool1_StayOn.ID, "Stay True");
+            idManager.SetName(bool1_SwitchOff.ID, "True to False");
+            idManager.SetName(bool1_SwitchOn.ID, "False to True");
+            #endregion
+            #region bool2
+            KnowInstance bool2_On = prod2.Get(disc2.GetBin(5.0).BinID);
+            KnowInstance bool2_Off = prod2.Get(disc2.GetBin(0.0).BinID);
+            KnowInstance bool2_StayOff = prod2.Get(bool2_Off, bool2_Off);
+            KnowInstance bool2_StayOn = prod2.Get(bool2_On, bool2_On);
+            KnowInstance bool2_SwitchOff = prod2.Get(bool2_On, bool2_Off);
+            KnowInstance bool2_SwitchOn = prod2.Get(bool2_Off, bool2_On);
+            idManager.SetName(bool2_On.ID, "True");
+            idManager.SetName(bool2_Off.ID, "False");
+            idManager.SetName(bool2_StayOff.ID, "Stay False");
+            idManager.SetName(bool2_StayOn.ID, "Stay True");
+            idManager.SetName(bool2_SwitchOff.ID, "True to False");
+            idManager.SetName(bool2_SwitchOn.ID, "False to True");
+            #endregion
+            #region booland
+            KnowInstance and_On = prodand.Get(discand.GetBin(5.0).BinID);
+            KnowInstance and_Off = prodand.Get(discand.GetBin(0.0).BinID);
+            KnowInstance and_StayOff = prodand.Get(and_Off, and_Off);
+            KnowInstance and_StayOn = prodand.Get(and_On, and_On);
+            KnowInstance and_SwitchedOff = prodand.Get(and_On, and_Off);
+            KnowInstance and_SwitchedOn = prodand.Get(and_Off, and_On);
+            idManager.SetName(and_On.ID, "True");
+            idManager.SetName(and_Off.ID, "False");
+            idManager.SetName(and_StayOff.ID, "Stay False");
+            idManager.SetName(and_StayOn.ID, "Stay True");
+            idManager.SetName(and_SwitchedOff.ID, "True to False");
+            idManager.SetName(and_SwitchedOn.ID, "False to True");
+            #endregion
+            #region boolor
+            KnowInstance or_On = prodor.Get(discor.GetBin(5.0).BinID);
+            KnowInstance or_Off = prodor.Get(discor.GetBin(0.0).BinID);
+            KnowInstance or_StayOff = prodor.Get(or_Off, or_Off);
+            KnowInstance or_StayOn = prodor.Get(or_On, or_On);
+            KnowInstance or_SwitchedOff = prodor.Get(or_On, or_Off);
+            KnowInstance or_SwitchedOn = prodor.Get(or_Off, or_On);
+            idManager.SetName(or_On.ID, "True");
+            idManager.SetName(or_Off.ID, "False");
+            idManager.SetName(or_StayOff.ID, "Stay False");
+            idManager.SetName(or_StayOn.ID, "Stay True");
+            idManager.SetName(or_SwitchedOff.ID, "True to False");
+            idManager.SetName(or_SwitchedOn.ID, "False to True");
+            #endregion
+            #region boolxor
+            KnowInstance xor_On = prodxor.Get(discxor.GetBin(5.0).BinID);
+            KnowInstance xor_Off = prodxor.Get(discxor.GetBin(0.0).BinID);
+            KnowInstance xor_StayOff = prodxor.Get(xor_Off, xor_Off);
+            KnowInstance xor_StayOn = prodxor.Get(xor_On, xor_On);
+            KnowInstance xor_SwitchedOff = prodxor.Get(xor_On, xor_Off);
+            KnowInstance xor_SwitchedOn = prodxor.Get(xor_Off, xor_On);
+            idManager.SetName(xor_On.ID, "True");
+            idManager.SetName(xor_Off.ID, "False");
+            idManager.SetName(xor_StayOff.ID, "Stay False");
+            idManager.SetName(xor_StayOn.ID, "Stay True");
+            idManager.SetName(xor_SwitchedOff.ID, "True to False");
+            idManager.SetName(xor_SwitchedOn.ID, "False to True");
             #endregion
 
-        }
+            //Convert policies to html and save to file
+            string htmlTree = HtmlTools.ToHtml(policyLearner, idManager);
+            string savePath = Path.Combine(this.ResultsDir, "LogicOperations", iterations + ".html");
+            File.WriteAllText(savePath, htmlTree);
 
-        [Fact]
-        public void blankaaa()
-        {
-            string dir = Environment.CurrentDirectory;
-            string name = this.GetType().Name;
+            return;
         }
     }
 }
